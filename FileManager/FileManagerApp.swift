a1c12e4f4615c6f28368816e592fe704253ee47d
David Whetstone <david@humblehacker.com>
2022-12-23 08:49:12 -0800

    Initial Commit

---
+++ FileManager/FileManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileManagerApp.swift
+//  FileManager
+//
+//  Created by David Whetstone on 12/23/22.
+//
+
+import SwiftUI
+
+@main
+struct FileManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
